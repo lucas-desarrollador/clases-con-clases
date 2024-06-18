547e0949ec1f8c2415a93d725f16ec23642c270c
LucasGongora <lucasgongora.dev@gmail.com>
2024-06-18 09:30:26 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+class Album{
+    title: string;
+    songs: string[];
+    constructor(title, songs:string[]){
+        this.title =  title;
+        this.songs = songs;
+    }
+}
+
+class Banda{
+    members: string[]
+    albums: Album[]
+    constructor(members:string[], albums:Album[]){
+        this.members = members;
+        this.albums = albums;
+    }
+}
+
+function main(){
+    const unAlbum = {
+        title: "album de lucas",
+        songs: ["una cancion"],
+    }
+
+    const unaBanda = new Banda(["lucas"], [unAlbum,unAlbum,unAlbum]);
+    //console.log(unaBanda.members);
+    console.log(unaBanda.albums);
+}
+
+main()
+2
